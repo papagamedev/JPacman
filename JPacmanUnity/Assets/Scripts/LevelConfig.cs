ade4b68a5dc5f8094cd40f0d6e931b85102a255d
Juan Pablo Lastra <juan.pablo.lastra.s@gmail.com>
2017-01-02 17:40:58 -0300

    Added LevelConfig scriptable object and an instance for Level01 map

---
+++ JPacmanUnity/Assets/Scripts/LevelConfig.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CreateAssetMenu(fileName="LevelConfig", menuName = "JPacman Data/Level Config")]
+public class LevelConfig : ScriptableObject {
+
+	[SerializeField]
+	Sprite m_background;
+
+	[SerializeField,Multiline(30)]
+	string m_map;
+
+}
