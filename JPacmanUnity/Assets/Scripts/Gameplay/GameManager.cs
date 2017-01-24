9a8113c5d2a4e2f7b2677cc469f18c7371cbdbba
Juan Pablo Lastra <juan.pablo.lastra.s@gmail.com>
2017-01-24 00:50:41 -0300

    Added Dots texture and sorting layer Added Dot, Player and Background prefabs Changed pixel to unit ratio of sprites to 16 Changed camera size to 15 Added code to read dots and player position from map Added GameManager class with InitLevel function to instantiate background, player and dots

---
+++ JPacmanUnity/Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+#region Serialized Fields
+
+	[SerializeField]
+	LevelConfig m_levelConfig;
+
+#endregion
+
+	GameObject[] m_dots;
+	GameObject m_player;
+
+	void Start () {
+
+		InitLevel ();
+
+	}
+	
+	void InitLevel () {
+	
+		var dotPrefab = m_levelConfig.DotPrefab;
+		var bgPrefab = m_levelConfig.BackgroundPrefab;
+		var playerPrefab = m_levelConfig.PlayerPrefab;
+		var map = m_levelConfig.Map;
+
+		// instantiate background
+		Vector3 centerPos = new Vector3(map.m_width*0.5f - 0.5f,map.m_height*0.5f - 2.0f,0);
+		Instantiate(bgPrefab,centerPos,Quaternion.identity,transform);
+
+		// instantiate dots
+		m_dots = new GameObject[map.m_dotPositions.Length];
+		int k = 0;
+		foreach (var dotPos in map.m_dotPositions) {
+
+			m_dots[k++] = Instantiate(dotPrefab,dotPos,Quaternion.identity,transform) as GameObject;
+		}
+
+		// instantiate the player
+		m_player = Instantiate(playerPrefab,map.m_playerPos,Quaternion.identity,transform) as GameObject;
+
+		// locate the camera
+		Vector3 cameraPos = centerPos;
+		cameraPos.z = -10.0f;
+		Camera.main.transform.position = cameraPos;
+	}
+
+}
---
+++ JPacmanUnity/Assets/Scripts/ScriptableObjects/LevelConfig.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName="LevelConfig", menuName = "JPacman Data/Level Config")]
+public class LevelConfig : ScriptableObject {
+
+#region Serialized Fields
+	[SerializeField]
+	GameObject m_backgroundPrefab;
+
+	[SerializeField]
+	GameObject m_dotPrefab;
+
+	[SerializeField]
+	GameObject m_playerPrefab;
+
+	[SerializeField,Multiline(30)]
+	string m_map;
+
+	public class MapData
+	{
+		public Vector2[] m_dotPositions;
+		public Vector2 m_playerPos;
+		public int m_width;
+		public int m_height;
+	}
+
+	MapData m_mapData;
+#endregion
+
+#region Public Accesors
+	public GameObject BackgroundPrefab
+	{
+		get
+		{
+			return m_backgroundPrefab;
+		}
+	}
+
+	public GameObject DotPrefab
+	{
+		get
+		{
+			return m_dotPrefab;
+		}
+	}
+
+	public GameObject PlayerPrefab
+	{
+		get
+		{
+			return m_playerPrefab;
+		}
+	}
+
+	public MapData Map
+	{
+		get
+		{
+			if (m_mapData == null)
+			{
+				m_mapData = LoadMap ();
+			}
+			return m_mapData;
+		}
+	}
+#endregion
+
+	MapData LoadMap()
+	{
+		var data = new MapData ();
+
+		// convert text into a chars matrix
+
+		string[] lines = m_map.Split ('\n');
+		int height = lines.Length;
+		int width = lines[0].Length;
+		char[,] mapChars = new char[width,height];
+
+		int x = 0, y = height - 1;
+		foreach (var line in lines) {
+			foreach (var c in line) {
+				mapChars [x, y] = c;
+				x++;
+			}
+			y--;
+			x = 0;
+		}
+
+		// now search for relevant elements in the map layout
+
+		var dots = new List<Vector2> ();
+		for (x = 0; x < width; x++) {
+			for (y = 0; y < height; y++) {
+
+				int c = mapChars [x, y];
+
+				switch (c) {
+				// check if there is a dot (group of four #)
+				case '#':
+					if (mapChars [x - 1, y] == '#' &&
+					    mapChars [x, y + 1] == '#' && mapChars [x - 1, y + 1] == '#') {
+						dots.Add (new Vector2 (x, y));
+					}
+					break;
+
+				// check for player start pos
+				case 'P':
+					data.m_playerPos = new Vector2 (x, y);
+					break;
+				}
+			}
+		}
+		data.m_width = width;
+		data.m_height = height;
+		data.m_dotPositions = dots.ToArray ();
+
+		return data;
+	}
+}
